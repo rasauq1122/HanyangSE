d0f31be9717d92c2222bf940727fce700e71f64e
rasauq1122 <rasauq1122@localhost>
2023-04-27 00:38:14 +0900

    leaf node 정렬하기 일부
    교수님이 주신 코드(ArrayList와 MutableTriple을 이용)는 class가 예상치 못하게 용량을 많이 차지하는 부분이
    있는 것 같습니다. 테스트 케이스 기준, 이론적으로는 30만개 정도의 레코드를 하나의 leaf node에 담을 수 있어야 하지만
    검증 결과 10만개 정도밖에 못 담는 것으로 보였습니다. (메모리 초과의 문제로)
    
    그래서 저는 최대한 class를 이용하지 않고 leaf node 정렬을 시도해보려고 합니다. 그러다보니 정렬하는 것도 직접 구현을
    해야 할 것 같습니다.
    
    추후 merge sort도 class없이 쌩구현으로 하는 것이 더 공간적으로 효율적일 것이라고 보입니다만..., 논의를 해보는
    것으로 하죠.

--- src/main/java/edu/hanyang/submit/HanyangSEExternalSort.java
+++ src/main/java/edu/hanyang/submit/HanyangSEExternalSort.java
@@ -1,8 +1,18 @@
 package edu.hanyang.submit;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import io.github.hyerica_bdml.indexer.ExternalSort;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import io.github.hyerica_bdml.indexer.ExternalSort;
+import org.apache.commons.lang3.tuple.MutableTriple;
 
 public class HanyangSEExternalSort implements ExternalSort {
 
@@ -15,8 +25,38 @@ public class HanyangSEExternalSort implements ExternalSort {
      * @param nblocks   Available block numbers in the main memory of the current system
      * @throws IOException  Exception while performing external sort
      */
+	
+	private int nxt_tmp_file = 0;
+	private int[][] leaf = null;
+	
+	private void leaf_quick_sort(int l, int r) {
+		// TODO: quicksort를 구현, leaf[0] ~ leaf[2]의 값을 순차적으로 비교하도록 한다.
+		
+	}
+	
+	
+	
     @Override
     public void sort(String infile, String outfile, String tmpdir, int blocksize, int nblocks) throws IOException {
         // TODO: your code here...
+    	
+    	// 1. initial phase : merge tree의 leaf node가 되도록 M block 단위로 정렬한다.
+    	System.out.println(blocksize + " * " + nblocks + " = " +  blocksize*nblocks);
+    	int leaf_length = (nblocks*blocksize)/(3*Integer.BYTES); // leaf node 하나의 얼마나 많은 record가 들어가는지]
+    	int leaf_size = leaf_length * (3 * Integer.BYTES); // leaf node 하나의 얼마나 많은 byte를 차지하는지 
+    	
+    	DataInputStream istream = new DataInputStream(new BufferedInputStream(new FileInputStream(infile), leaf_size));
+    	long remain = 0;
+    	while ((remain = istream.available()) > 0) {
+    		int now_size = (int) (remain > leaf_size ? (leaf_length) : remain/(3 * Integer.BYTES));
+    		leaf = new int[3][now_size];
+    		for (int i = 0 ; i < now_size ; i++) {
+    			for (int j = 0 ; j < 3 ; j++) leaf[j][i] = istream.readInt();
+    		}
+    		
+    		leaf = null;
+    	}
+    	
+    	istream.close();
     }
 }
